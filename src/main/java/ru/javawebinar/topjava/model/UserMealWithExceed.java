3e3bfb13ec19388599eb3f3cc67dba07dc56a583
aveugene <eugeneavilov@gmail.com>
2019-05-05 18:32:58 +0300

    HW0. forEach and Stream API usage.

--- src/main/java/ru/javawebinar/topjava/model/UserMealWithExceed.java
+++ src/main/java/ru/javawebinar/topjava/model/UserMealWithExceed.java
@@ -17,4 +17,16 @@ public class UserMealWithExceed {
         this.calories = calories;
         this.exceed = exceed;
     }
+
+    public UserMealWithExceed(UserMeal meal, boolean exceed) {
+        this.dateTime = meal.getDateTime();
+        this.description = meal.getDescription();
+        this.calories = meal.getCalories();
+        this.exceed = exceed;
+    }
+
+    @Override
+    public String toString() {
+        return dateTime + " " + description + " " + calories + " " + exceed;
+    }
 }
