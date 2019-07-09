cf894e17e21185958a38d5c8f656b765e40f8600
aveugene <eugeneavilov@gmail.com>
2019-07-09 17:56:02 +0300

    HW05 7.1. Get user with his meals for DataJpa realisation only. Ignored meals in tests.

--- src/test/java/ru/javawebinar/topjava/service/DataJpa/DataJpaUserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/DataJpa/DataJpaUserServiceTest.java
@@ -1,9 +1,23 @@
 package ru.javawebinar.topjava.service.DataJpa;
 
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ActiveProfiles;
 import ru.javawebinar.topjava.Profiles;
+import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.service.UserService;
 import ru.javawebinar.topjava.service.UserServiceTest;
+
+import static ru.javawebinar.topjava.UserTestData.*;
 
 @ActiveProfiles(Profiles.DATAJPA)
 public class DataJpaUserServiceTest extends UserServiceTest {
+    @Autowired
+    private UserService service;
+
+    @Test
+    public void getWithMeals() throws Exception {
+        User user = service.getWithMeals(ADMIN_ID);
+        assertMatch(user, ADMIN);
+    }
 }
