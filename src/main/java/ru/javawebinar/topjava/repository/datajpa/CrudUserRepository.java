b59714625a7e8ef7c919de9490b8f89e52bb9f41
aveugene <eugeneavilov@gmail.com>
2019-07-24 02:09:49 +0300

    HW06 corr1. datajpa user getWithMeals with roles.

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudUserRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudUserRepository.java
@@ -18,6 +18,6 @@ public interface CrudUserRepository extends JpaRepository<User, Integer> {
     User getByEmail(String email);
 
     @EntityGraph(attributePaths = {"meals", "roles"})
-    @Query("SELECT u FROM User u WHERE u.id=?1")
+    @Query("SELECT u FROM User u LEFT JOIN FETCH u.roles WHERE u.id=?1")
     User getWithMeals(int id);
 }
