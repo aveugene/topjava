224ee8ce9be0ef19c219c6c7faabd0ba926b87be
aveugene <eugeneavilov@gmail.com>
2019-07-25 01:38:16 +0300

    HW06 corr2. JdbcUserRepository return null if updated user does not exist.

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcUserRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcUserRepository.java
@@ -47,11 +47,12 @@ public class JdbcUserRepository implements UserRepository {
         if (user.isNew()) {
             Number newKey = insertUser.executeAndReturnKey(parameterSource);
             user.setId(newKey.intValue());
-        } else  {
-//            TODO : if update user which is not exist return null
-            namedParameterJdbcTemplate.update(
+        } else {
+            if (namedParameterJdbcTemplate.update(
                     "UPDATE users SET name=:name, email=:email, password=:password, " +
-                            "registered=:registered, enabled=:enabled, calories_per_day=:caloriesPerDay WHERE id=:id", parameterSource);
+                            "registered=:registered, enabled=:enabled, calories_per_day=:caloriesPerDay WHERE id=:id", parameterSource) == 0) {
+                return null;
+            }
             jdbcTemplate.update("DELETE FROM user_roles WHERE user_id=?", user.getId());
         }
         updateRoles(user);
@@ -92,9 +93,9 @@ public class JdbcUserRepository implements UserRepository {
 
     private void updateRoles(User user) {
         Set<Role> roleSet = user.getRoles();
-        if(!CollectionUtils.isEmpty(roleSet)) {
+        if (!CollectionUtils.isEmpty(roleSet)) {
             jdbcTemplate.batchUpdate("INSERT INTO user_roles (user_id, role) VALUES (?, ?)", roleSet, roleSet.size(),
-                    (ps,role) -> {
+                    (ps, role) -> {
                         ps.setInt(1, user.getId());
                         ps.setString(2, role.name());
                     });
--- src/test/java/ru/javawebinar/topjava/service/jdbc/JdbcUserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/jdbc/JdbcUserServiceTest.java
@@ -1,10 +1,20 @@
 package ru.javawebinar.topjava.service.jdbc;
 
+import org.junit.Test;
 import org.springframework.test.context.ActiveProfiles;
+import ru.javawebinar.topjava.model.Role;
+import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.service.AbstractUserServiceTest;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
 
 import static ru.javawebinar.topjava.Profiles.JDBC;
 
 @ActiveProfiles(JDBC)
 public class JdbcUserServiceTest extends AbstractUserServiceTest {
+
+    @Test(expected = NotFoundException.class)
+    public void updateNotFound() throws Exception {
+        User updated = new User(2, "User1", "user1@yandex.ru", "password", Role.ROLE_USER);
+        service.update(updated);
+    }
 }
