1bf22c59aefb89219291dc10c68b7993143898a1
aveugene <eugeneavilov@gmail.com>
2019-08-13 01:45:05 +0300

    No NotFoundException for User and MealService.

--- src/main/java/ru/javawebinar/topjava/service/UserService.java
+++ src/main/java/ru/javawebinar/topjava/service/UserService.java
@@ -88,7 +88,7 @@ public class UserService implements UserDetailsService {
         return new AuthorizedUser(user);
     }
 
-    public User getWithMeals(int id) throws NotFoundException {
+    public User getWithMeals(int id) {
         return checkNotFoundWithId(repository.getWithMeals(id), id);
     }
 }
